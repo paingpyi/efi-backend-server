bf0547ba331a8a4821257f5b6ffc7eddac949136
KYIMINHAN <kyiminhan@KYIMINHANs-MacBook-Air.local>
2022-04-19 21:24:29 +0700

    Update UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="{{ asset('/storage/uploads/demo/favicon.svg') }}" alt="EFI Logo" style="opacity: .8">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
