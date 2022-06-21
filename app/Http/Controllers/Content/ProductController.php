60fb0f376dccd10de86b0d0d34d92030c0392272
KYIMINHAN <kyiminhan@KYIMINHANs-MacBook-Air.local>
2022-06-21 10:19:51 +0700

    Test result on console

--- app/Http/Controllers/Content/ProductController.php
+++ app/Http/Controllers/Content/ProductController.php
@@ -899,8 +899,8 @@ class ProductController extends Controller
             ]
         ]);
         // End of Product Updates
-
-        return redirect()->route('product#list')->with(['success_message' => 'Successfully <strong>updated!</strong>' . ($response->status() >= 200 and $response->status() >= 299? $old_product->slug_url:'<br><span class="text-danger">Error on response Frontend</span>')]);
+dd($old_product);
+        return redirect()->route('product#list')->with(['success_message' => 'Successfully <strong>updated!</strong>' . (($response->status() >= 200 and $response->status() >= 299)? $old_product->slug_url:'<br><span class="text-danger">Error on response Frontend</span>')]);
     }
 
     /**
